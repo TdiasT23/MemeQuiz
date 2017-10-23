f88a17d683a0de762de379378dfcbaa1db22ed83
Vinicius Nunes <viniiciuspereiira@hotmail.com>
2017-10-23 15:46:30 -0200

    Tela de questão criada, botão responder mudando de tela

--- app/src/main/java/com/example/viniciusnunes/memequiz/QuizActivity.java
+++ app/src/main/java/com/example/viniciusnunes/memequiz/QuizActivity.java
@@ -1,7 +1,10 @@
 package com.example.viniciusnunes.memequiz;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioGroup;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+    }
+
+    public void btnResponderOnClick(View v){
+        RadioGroup rgRespostas = (RadioGroup)findViewById(R.id.rgRespostas);
+        Intent intent = new Intent(this, RespostaActivity.class);
+        intent.putExtra("acertou", rgRespostas.getCheckedRadioButtonId() == R.id.rbResposta1);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/viniciusnunes/memequiz/RespostaActivity.java
@@ -0,0 +1,13 @@
+package com.example.viniciusnunes.memequiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RespostaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resposta);
+    }
+}

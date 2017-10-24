69f72e0ba1a0374e685ec0aabb973306899df9c8
Vinicius Nunes <viniiciuspereiira@hotmail.com>
2017-10-24 19:18:57 -0200

    Tela de perguntas exibe Erros e Acertos (Estático)

--- app/src/main/java/com/example/viniciusnunes/memequiz/RespostaActivity.java
+++ app/src/main/java/com/example/viniciusnunes/memequiz/RespostaActivity.java
@@ -1,7 +1,10 @@
 package com.example.viniciusnunes.memequiz;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RespostaActivity extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class RespostaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resposta);
+
+        ImageView imgResposta = (ImageView)findViewById(R.id.imgResposta);
+        TextView resposta = (TextView)findViewById(R.id.resposta);
+
+        Intent intent = getIntent();
+        boolean acertou = intent.getBooleanExtra("acertou", false);
+        if(acertou){
+            imgResposta.setImageResource(R.drawable.correct);
+            resposta.setText("Acertou!");
+        }
+        else{
+            imgResposta.setImageResource(R.drawable.incorrect);
+            resposta.setText("Errou!");
+        }
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finish();
+            }
+        });
+        thread.start();
     }
 }

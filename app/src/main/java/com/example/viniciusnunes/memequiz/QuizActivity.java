cfa739221a49d64f424003f39b6280660ac5a0d9
Vinicius Nunes <viniiciuspereiira@hotmail.com>
2017-10-25 23:26:00 -0200

    Quiz concluído

--- app/src/main/java/com/example/viniciusnunes/memequiz/QuizActivity.java
+++ app/src/main/java/com/example/viniciusnunes/memequiz/QuizActivity.java
@@ -22,16 +22,16 @@ public class QuizActivity extends AppCompatActivity {
     int pontos;
     List<Questao> questoes = new ArrayList<Questao>(){
         {
-            add(new Questao(R.drawable.burro, "Não cara...", R.id.rbResposta2, "Como você é ridículo!", "Como você é burro!", "Como você é idiota!", "Como você é pífio"));
+            add(new Questao(R.drawable.morre, "O suspeito acima ficou chateado com as perguntas e falou para os reporteres", R.id.rbResposta1, "Morre, diabo!", "Quero que todos saiam daqui!", "Vão todos tomar no c*", "Vão se foderem"));
             add(new Questao(R.drawable.choque, "Aqui por exemplo, temos...", R.id.rbResposta4, "Uiiii uii ui", "Ai caramba!", "Ai meu Deus!", "Aiii ai aiii"));
             add(new Questao(R.drawable.gostozinho, "Segundo a pensadora TN Martins, o novinho está", R.id.rbResposta1, "Gostozinho no azeite", "Mec Nelson nos acessos", "Brotando a xota", "Aquelas coisas"));
             add(new Questao(R.drawable.poha, "O comentarista informou aos telespectadores que o programa:", R.id.rbResposta3, "Ta uma merda", "Ta uma droga", "Ta uma poha", "Ta uma zorra"));
-            add(new Questao(R.drawable.cremoso, "Complete a frase segundo Gilderlan. E depois..tem:", R.id.rbResposta1, "Xenhenhenhenhem", "Nheco Nheco", "Nananananana", "Um furdunço"));
+            add(new Questao(R.drawable.burro, "Não cara...", R.id.rbResposta2, "Como você é ridículo!", "Como você é burro!", "Como você é idiota!", "Como você é pífio"));
             add(new Questao(R.drawable.pao, "Mamãe, no céu tem pão?", R.id.rbResposta3, "E sobreviveu", "E viveu", "E morreu", "E faleceu"));
-            add(new Questao(R.drawable.mamilos, "Hoje eu vim falar de um assunto polêmico", R.id.rbResposta4, "Nádegas", "Olhos", "Mãos", "Mamilos!"));
-            add(new Questao(R.drawable.lohany, "Qual é o nome artistico dessa maravilhosa criatura?", R.id.rbResposta2, "Lohane Vêkanandre Gisele Gavião da Portela de bala Halls", "Lohane Vêkanandre Sthephany Smith Bueno de HA HA HA de Raio Laser bala de Icekiss", "Lohane Vêkanandre Bueno de HA HA HA de Raio Laser de bala Mentos", "Lohane Vêkanandre Sthephany Smith Brasil de Paitê"));
-            add(new Questao(R.drawable.morre, "O suspeito acima, ficou chateado com as perguntas e falou para os reporteres", R.id.rbResposta1, "Morre, diabo!", "Quero que todos saiam daqui!", "Vão todos tomar no c*", "Vão se foderem"));
-            add(new Questao(R.drawable.jesus, "A missionária estava pregando sua palavra e disse que:", R.id.rbResposta3, "Sem Jesus não viverão!", "Jesus é bom o tempo todo", "O sangue de Jesus tem poder!", "O sangue de Jesus é maravilhosooooo!"));
+            add(new Questao(R.drawable.mamilos, "Hoje eu vim falar de um assunto polêmico..", R.id.rbResposta4, "Nádegas", "Olhos", "Mãos", "Mamilos"));
+            add(new Questao(R.drawable.cremoso, "Complete a frase segundo Gilderlan. E depois.. tem:", R.id.rbResposta1, "Xenhenhenhenhem", "Nheco Nheco", "Nananananana", "Um furdunço"));
+            add(new Questao(R.drawable.lohany, "Qual é o nome artístico dessa maravilhosa criatura?", R.id.rbResposta2, "Lohane Vêkanandre Gisele Gavião da Portela de bala Halls", "Lohane Vêkanandre Sthephany Smith Bueno de HA HA HA de Raio Laser bala de Icekiss", "Lohane Vêkanandre Bueno de HA HA HA de Raio Laser de bala Mentos", "Lohane Vêkanandre Sthephany Smith Brasil de Paitê"));
+            add(new Questao(R.drawable.jesus, "Qual o refrão da música que essa missionária estava cantando?", R.id.rbResposta3, "Sem Jesus não viverão!", "Jesus é bom o tempo todo", "O sangue de Jesus tem poder!", "O sangue de Jesus é maravilhosooooo!"));
 
 
         }
